7719197a377fc1293dd9902c2bad4ce4c425e318
P2949 <torbypedro@gmail.com>
2026-09-06 09:30:35 +0100

    fix top-level sandbox probe status variable

--- tests/optimization/test-portage-sample-pgo-integration.sh
+++ tests/optimization/test-portage-sample-pgo-integration.sh
@@ -2570,7 +2570,7 @@ if [[ ${PORTAGE_POLICY_MODE} == live ]]; then
         > "${WORK}/sandbox-enforcement.tsv"
     write_live_policy_probe_environment
     run_ebuild "${WORK}/sandbox-probe-clean.log" clean
-    local sandbox_probe_status=0
+    sandbox_probe_status=0
     set +e
     run_ebuild "${WORK}/sandbox-probe-build.log" compile
     sandbox_probe_status=$?
