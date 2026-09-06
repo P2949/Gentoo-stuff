ce632f41e34f0dad1de77061406d8aa5fafd17c1
P2949 <torbypedro@gmail.com>
2026-09-06 09:33:29 +0100

    contain expected sandbox probe error status

--- tests/optimization/test-portage-sample-pgo-integration.sh
+++ tests/optimization/test-portage-sample-pgo-integration.sh
@@ -2571,10 +2571,12 @@ if [[ ${PORTAGE_POLICY_MODE} == live ]]; then
     write_live_policy_probe_environment
     run_ebuild "${WORK}/sandbox-probe-clean.log" clean
     sandbox_probe_status=0
+    trap '' ERR
     set +e
     run_ebuild "${WORK}/sandbox-probe-build.log" compile
     sandbox_probe_status=$?
     set -e
+    trap - ERR
     if ((sandbox_probe_status == 0)); then
         fail 'live Portage sandbox allowed its DAC-permitted external write probe'
     fi
