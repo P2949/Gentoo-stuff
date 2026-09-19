bb863a8e2b46aaf2880b8fc6f057959414013e77
P2949 <torbypedro@gmail.com>
2026-09-19 10:17:57 +0100

    isolate installer child profile working directory

--- tests/optimization/test-framework-installer.sh
+++ tests/optimization/test-framework-installer.sh
@@ -993,6 +993,7 @@ run_check() {
     GENTOO_OPT_INSTALLER_TEST_MODE=1 \
     GENTOO_OPT_PRODUCTION_PROFILE_TRANSACTION_TOKEN=\${supplied_token} \
     GENTOO_OPT_PRODUCTION_PROFILE_TRANSACTION_AUTHORIZATION=\${supplied_authorization} \
+    cd /tmp
     bash -- ${REPOSITORY@Q}/scripts/optimization/install-framework.sh \
         --test-root ${TARGET@Q} --check
 }
