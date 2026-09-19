af06aca2c1ca08ab5c877e861ceb4d62f3f89802
P2949 <torbypedro@gmail.com>
2026-09-19 13:27:20 +0100

    suppress implicit profile in bolt capture wrapper

--- scripts/optimization/bolt/capture-input.sh
+++ scripts/optimization/bolt/capture-input.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export LLVM_PROFILE_FILE=/dev/null
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 unset PYTHONHOME PYTHONPATH PYTHONSTARTUP PYTHONINSPECT
